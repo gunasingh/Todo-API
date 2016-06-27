66871d3a20a29edd246956f5d82ed2c1c7818859
Gunasingh Chidambaram <gunasingh.1@gmail.com>
2016-06-26 17:06:38 -0700

    Final static todos

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
-// GET /todos
+// GET /todos?completed=false&q=work
 app.get('/todos', function(req, res) {
     var queryParams = req.query;
     var filteredTodos = todos;
@@ -22,8 +22,15 @@ app.get('/todos', function(req, res) {
         filteredTodos = _.where(filteredTodos, {completed: true});
     } else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
         filteredTodos = _.where(filteredTodos, {completed: false});
-    } else {
-        return res.status(400).send();
+    } 
+
+    if (queryParams.hasOwnProperty('q') && queryParams.q.trim().length > 0) {
+        filteredTodos = _.filter(filteredTodos, function(todo) {
+            if (todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1) {
+                return true;
+            }
+            return false;
+        });
     }
 
     res.json(filteredTodos);
