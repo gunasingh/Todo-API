39ef61dd5d68ba60383a6bce782ad57c96de508b
Guna Singh Chidambaram <gunasingh.1@gmail.com>
2016-06-29 07:58:46 -0700

    Hash and salt user passwords!

--- models/user.js
+++ models/user.js
@@ -1,3 +1,6 @@
+var bcrypt = require('bcrypt');
+var _ = require('underscore');
+
 module.exports = function(sequelize, DataTypes) {
     return sequelize.define('user', {
         email: {
@@ -8,11 +11,26 @@ module.exports = function(sequelize, DataTypes) {
                 isEmail: true
             }
         },
+        salt: {
+            type: DataTypes.STRING
+        },
+        password_hash: {
+            type: DataTypes.STRING
+        },
         password: {
-            type: DataTypes.STRING,
+            type: DataTypes.VIRTUAL,
             allowNull: false,
             validate: {
                 len: [7, 100]
+            },
+            // done when user object is set on server side before putting into db
+            set: function (value) {
+                var salt = bcrypt.genSaltSync(10);
+                var hashedPassword = bcrypt.hashSync(value, salt);
+
+                this.setDataValue('password', value);
+                this.setDataValue('salt', salt);
+                this.setDataValue('password_hash', hashedPassword);
             }
         }
     }, {
@@ -21,6 +39,13 @@ module.exports = function(sequelize, DataTypes) {
                 if (typeof user.email === 'string') {
                     user.email = user.email.toLowerCase();
                 }
+            }
+        },
+        // to prevent displaying password, salt and passwordHash to user after creation/update
+        instanceMethods: {
+            toPublicJSON: function () {
+                var json = this.toJSON();
+                return _.pick(json, 'id', 'email', 'updatedAt', 'createdAt');
             }
         }
     });
