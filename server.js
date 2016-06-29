c8847ca300b258370e0df001b5e3d4d0f0100d03
Guna Singh Chidambaram <gunasingh.1@gmail.com>
2016-06-28 22:54:55 -0700

    Update PUT /todos/:id to use Sequelize

--- server.js
+++ server.js
@@ -140,7 +140,7 @@ app.delete('/todos/:id', function(req, res) {
 // PUT /todos/:id
 app.put('/todos/:id', function(req, res) {
     var todoId = parseInt(req.params.id, 10);
-    var matchedTodo = _.findWhere(todos, {
+    /*var matchedTodo = _.findWhere(todos, {
         id: todoId
     });
     var body = _.pick(req.body, 'description', 'completed');
@@ -166,8 +166,32 @@ app.put('/todos/:id', function(req, res) {
 
     // HERE
     _.extend(matchedTodo, validAttributes);
-    res.json(matchedTodo);
+    res.json(matchedTodo);*/
 
+    var body = _.pick(req.body, 'description', 'completed');
+    var attributes = {};
+
+    if (body.hasOwnProperty('completed')) {
+        attributes.completed = body.completed;
+    }
+
+    if (body.hasOwnProperty('description')) {
+        attributes.description = body.description;
+    }
+
+    db.todo.findById(todoId).then(function (todo) {
+        if (todo) {
+            todo.update(attributes).then(function (todo) {
+                res.json(todo.toJSON());
+            }, function (e) {
+                res.status(400).json(e);
+            })
+        } else {
+            res.status(404).send();
+        }
+    }, function() {
+        res.status(500).send();
+    });
 });
 
 db.sequelize.sync().then(function() {
